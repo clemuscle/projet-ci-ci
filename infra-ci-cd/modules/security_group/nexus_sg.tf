4b10170580480ac5e594f0ec6045924f79738952
clement <clementmoabenedetti@gmail.com>
2025-02-08 17:07:17 +0000

    Communication en private subnet et public subnet fonctionnelle
    Test de ping de l'instance wireguard vers l'instance gitlab et nexus
    Pas encore via la connexion VPN

--- infra-ci-cd/modules/security_group/nexus_sg.tf
+++ infra-ci-cd/modules/security_group/nexus_sg.tf
@@ -4,6 +4,22 @@ resource "aws_security_group" "nexus_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "From WireGuard VPN subnet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    description = "From WireGuard VPN clients subnet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.8.0.0/24"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
--- infra-ci-cd/modules/security_group/wireguard_sg.tf
+++ infra-ci-cd/modules/security_group/wireguard_sg.tf
@@ -4,6 +4,14 @@ resource "aws_security_group" "wireguard_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "From private subnet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
